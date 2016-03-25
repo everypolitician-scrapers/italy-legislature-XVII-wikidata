a46163e60bc2a6b228551bb2d2973b3f83f5234c
Tony Bowden <tony@tmtm.com>
2016-03-25 10:12:04 +0000

    fetch in batches of 250

--- scraper.rb
+++ scraper.rb
@@ -4,6 +4,4 @@
 require 'wikidata/fetcher'
 
 names = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/italy-legislature-XVII-wikipedia', column: 'wikiname')
-names.shuffle.each_slice(250) do |sliced|
-  EveryPolitician::Wikidata.scrape_wikidata(names: { it: sliced })
-end
+EveryPolitician::Wikidata.scrape_wikidata(names: { it: names }, batch_size: 250)
