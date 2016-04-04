61b71e3cd5b0b455f423c6c37cbc220e6e4f5bb1
Tony Bowden <tony@tmtm.com>
2016-04-04 09:12:54 +0100

    fetch in batches of 100

--- scraper.rb
+++ scraper.rb
@@ -6,4 +6,4 @@ require 'wikidata/fetcher'
 names = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/italy-legislature-XVII-wikipedia', column: 'wikiname')
 by_cat = WikiData::Category.new( 'Categoria:Deputati della XVII Legislatura della Repubblica Italiana', 'it').member_titles
 
-EveryPolitician::Wikidata.scrape_wikidata(names: { it: names | by_cat }, batch_size: 250)
+EveryPolitician::Wikidata.scrape_wikidata(names: { it: names | by_cat }, batch_size: 100)
