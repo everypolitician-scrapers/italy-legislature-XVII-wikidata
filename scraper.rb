52eb3a84dcd7deb2d75559c80e49be47440620f7
Tony Bowden <tony@tmtm.com>
2016-10-01 09:32:00 +0100

    Fail noisily if we have no category members

--- scraper.rb
+++ scraper.rb
@@ -6,4 +6,6 @@ require 'wikidata/fetcher'
 names = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/italy-legislature-XVII-wikipedia', column: 'wikiname')
 by_cat = WikiData::Category.new( 'Categoria:Deputati della XVII Legislatura della Repubblica Italiana', 'it').member_titles
 
+abort "No members in category" if by_cat.empty?
+
 EveryPolitician::Wikidata.scrape_wikidata(names: { it: names | by_cat }, batch_size: 100)
