75dd49b6322bbb1a1f5aaba6f67b87238d364a37
Tony Bowden <tony@tmtm.com>
2016-12-24 08:00:58 +0000

    Try getting _only_ the Category members
    The people listed on the Wikipedia page who _aren't_ in the Category
    seem to all be redirects.

--- scraper.rb
+++ scraper.rb
@@ -3,9 +3,8 @@
 
 require 'wikidata/fetcher'
 
-names = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/italy-legislature-XVII-wikipedia', column: 'wikiname')
+# names = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/italy-legislature-XVII-wikipedia', column: 'wikiname')
 by_cat = WikiData::Category.new( 'Categoria:Deputati della XVII legislatura della Repubblica Italiana', 'it').member_titles
-
 abort "No members in category" if by_cat.empty?
 
-EveryPolitician::Wikidata.scrape_wikidata(names: { it: names | by_cat })
+EveryPolitician::Wikidata.scrape_wikidata(names: { it: by_cat })
